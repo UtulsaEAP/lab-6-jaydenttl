d0133659ddf79f7a792ac708f9a2a25c7828da7f
Jayden Ly <jtl5627@utulsa.edu>
2024-03-07 12:08:58 -0600

    5

--- Problem#5/elementsrange.py
+++ Problem#5/elementsrange.py
@@ -1,14 +1,42 @@
-def filter_and_print_range(input_list, min_val, max_val):
+'''
+Name: Jayden Ly
+Lab Time: 2/29/24 5:30 PM
+def process_and_print(input_string):
+    # Split into separate strings
+  all_num = input_string.split()
+    # Convert strings to integers and filter out negative values
+  int_num = list(map(int, all_num))
+  neg_num = list(filter(lambda n: n < 0,int_num))
+    # Sort integers in reverse order
+  neg_num.sort(reverse=True)
+    # Print sorted integers
+  print(' '.join(map(str, neg_num))) 
+'''
+def filter_and_print_range(integer_list, min_val, max_val):
     #write your code here
 
-if __name__ == '__main__':
+
+    filtered_list = list(filter(lambda n: min_val <= n <= max_val, integer_list))
+    restr_list = list(map(str, filtered_list))
+    comma_list = []
+    for _ in range(len(restr_list)):
+        comma_list += ','
+
+    list_w_comma = [restr_list+comma_list for restr_list, comma_list in zip(restr_list, comma_list)]
+    print(''.join(map(str,list_w_comma)))
+    
+
+
+if __name__ == "__main__":
     # Get input for the list of integers
     user_input = input("Enter a space-separated string of numbers: ")
-    integer_list =
+    integer_list = list(map(int, user_input.split()))
 
     # Get input for the range (min and max values)
     user_input = input("Enter the min and max values separated by a space: ")
-    min_val, max_val = 
+    min_val, max_val = list(map(int, (user_input.split())))
+
+    filter_and_print_range(integer_list, min_val, max_val)
 
     # Call the function to filter and print the numbers in the given range
    
