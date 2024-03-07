8dfa3b8364a6b4b5196285d29dc0d47377a90652
Jayden Ly <jtl5627@utulsa.edu>
2024-03-07 12:42:41 -0600

    6

--- Problem#6/contactlist.py
+++ Problem#6/contactlist.py
@@ -1,14 +1,23 @@
+'''
+Name: Jayden Ly
+Lab Time: 3/7/24 12:13 PM
+'''
 def process_user_contacts(user_input):
-    user_contacts = 
+    user_contacts = {}
 
- 
-    user_input = 
-    tokens = 
+    user_input = user_input.replace(" ", ",") # replace all space with comma
 
+    split_input = user_input.split(',') # split by all commars
     # Put word pairs into a dictionary
-    
+    for i in range(0, len(split_input), 2):
+        name = split_input[i]                   #first item is name
+        phone_number = split_input[i + 1]       #item +1 so second item is phone
+        user_contacts[name] = phone_number      #dictionary returns phone if u do this
     # Get contact name from input, output contact's phone number
     contact_name = input("Enter the contact name: ")
+    
+    print(user_contacts[contact_name])          #FIYTB!!!
+
     
    
 if __name__ == '__main__':
